4f25dec6fa5014112ad27458a1c4df5e4bd61112
lovelotus <opera.prem@gmail.com>
2022-07-18 23:21:33 +0530

    Added LeapYear Program

---
+++ LeapYear/LeapYear/Program.cs
@@ -0,0 +1,9 @@
+﻿int year;
+Console.WriteLine("Enter the Year");
+year = Convert.ToInt32(Console.ReadLine());
+if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    Console.WriteLine("Year is Leap Year");
+else
+    Console.WriteLine("Year is not a Leap Year");
+
+Console.ReadKey();
