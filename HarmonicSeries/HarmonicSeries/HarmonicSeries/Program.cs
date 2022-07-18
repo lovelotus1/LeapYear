fe1df1db92d00363259109c41418e5cd628ba6c0
lovelotus <opera.prem@gmail.com>
2022-07-18 23:31:11 +0530

    Added HarmonicSeries Program

---
+++ HarmonicSeries/HarmonicSeries/HarmonicSeries/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the value N");
+int n = int.Parse(Console.ReadLine());
+float h = 0;
+for (int i = 0; i < n; i++)
+{
+    h += (float)1 / i;
+}
+Console.WriteLine(" Nth value of harmoic number is " + "" + h);
